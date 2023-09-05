f4a081e20d419a1f44dad938f412eef96849e992
Your Name <ilyos.sultanov2020@gmail.com>
2023-09-05 15:39:03 +0500

    .

---
+++ Array/1346.Check_if_N_and_Its_Double_Exist.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const currentValue = arr[i];
+    for (let j = 0; j < arr.length; j++) {
+      const possibleValue = arr[j];
+      if (possibleValue === 2 * currentValue && i !== j) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
